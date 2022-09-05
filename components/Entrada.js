c84fa6888658b83f747494354a6734590da4b7cb
Bastian1212 <bastianvillanueva@me.com>
2022-09-05 14:42:07 -0400

    agregando la imagen al blog

--- components/Entrada.js
+++ components/Entrada.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
 import { formatearFecha } from '../helpers';
 const Entrada = ({entrada}) => {
     const {titulo,resumen,imagen, published_at ,id } = entrada;
   return (
     <article>
+        <Image  layout='responsive' width={800} height={600} src={imagen.url} alt={`imagrn blog ${titulo}`}/>
         <div>
             <h1>{titulo}</h1>
             <p>{formatearFecha(published_at)}</p>
             <p>{resumen}</p>
+            <Link href={`/blog/${id}`}>
+                Leer Entrada
+            </Link>
 
         </div>
         
