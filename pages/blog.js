aedb256033943160a3054ad6876b76ff50128549
Bastian1212 <bastianvillanueva@me.com>
2022-09-05 12:35:42 -0400

    consultado a la api

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,18 @@
+import react, {useEffect} from "react"
+
 import Layout from "../components/Layout/Layout"
 
 const Blog = () => {
+    useEffect(() => {
+       const  consultarApi = async () =>{
+            const url = 'http://localhost:1337/blogs/'
+            const respuesta  = await fetch(url)
+            const resultado = await respuesta.json()
+            console.log(resultado);
+
+        }
+        consultarApi();
+    }, [])
     return(
         <Layout
             pagina={"Blog"}
