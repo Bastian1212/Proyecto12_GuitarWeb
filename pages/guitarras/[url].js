6023a48349459fba46ad539f39a55354bfc5d3f9
Bastian1212 <bastianvillanueva@me.com>
2022-09-06 16:01:45 -0400

    agrgar producto  para el carrito

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -17,8 +17,22 @@ const Producto = ({guitarra}) => {
                     <p className={styled.descripcion}>{descripcion}</p>
                     <p className={styled.precio}>${precio}</p>
 
-                    <form>
+                    <form className={styled.formulario}>
                         <label>Cantidad :</label>
+                        <select>
+                            <option value={""}>-- Seleccione --</option>
+                            <option value={"1"}>1</option>
+                            <option value={"2"}>2</option>
+                            <option value={"3"}>3</option>
+                            <option value={"4"}>4</option>
+                            <option value={"5"}>5</option>
+                            <option value={"6"}>6</option>
+                            <option value={"7"}>7</option>
+                        </select>
+                        <input
+                            type="submit"
+                            value="Agregar al Carrito "
+                        />
                     </form>
                 </div>
             </div>
