121c32bc75c54f312da2010b6a52e53828148774
Bastian1212 <bastianvillanueva@me.com>
2022-09-07 10:58:31 -0400

    avance

--- components/Curso.js
+++ components/Curso.js
@@ -1,19 +1,31 @@
 import React from 'react'
 
+import styled from '../styles/Curso.module.css'
+
+
 const Curso = (cursos) => {
     const {titulo,contenido,imagen} = cursos.cursos;
     
     return (
-        <section>
-            <div  className='contenedor'>
-                <div>
+        <section >
+            <div  className={`contenedor ${styled.grid}`}>
+                <div className={styled.contenido}>
                     
-                    <h2>{titulo}</h2>
-                    <p>{contenido}</p>
+                    <h2 className='heading'>{titulo}</h2>
+                    <p className={styled.texto}>{contenido}</p>
 
-                    <a href='#'>Mas Información</a>
+                    <a className={styled.enlace} href='#'>Mas Información</a>
                 </div>
             </div>
+            <style jsx>{`
+                section {
+                    padding: 10rem 0 ;
+                    margin-top: 10rem;
+                    background-image: linear-gradient(to right, rgb(0 0 0 / .65), rgb( 0 0 0 / .7)), url(${imagen.url});
+                    background-size: cover;
+                    background-position: 50%;
+                }
+            `}</style>
         </section>
     )
 }
